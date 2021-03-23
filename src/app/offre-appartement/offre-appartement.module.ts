c30ed444d9482d8dfc6012a795fbf79238403a9a
Amira Mostapha <mostaphaamira2@gmail.com>
2021-03-23 11:46:38 +0100

    Ok to delete

---
+++ src/app/offre-appartement/offre-appartement.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OffreAppartementRoutingModule } from './offre-appartement-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OffreAppartementRoutingModule
+  ]
+})
+export class OffreAppartementModule { }
