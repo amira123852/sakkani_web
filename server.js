5ef42fcc05c29b2443161f565944a48afdfd524d
Amira Mostapha <mostaphaamira2@gmail.com>
2021-03-23 13:58:21 +0100

    fix server dist

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var app = express();
 app.use(compression());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/custom-feed'));
+app.use(express.static(__dirname + '/dist/sakkani'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/custom-feed/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/sakkani/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
