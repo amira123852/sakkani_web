c709de0d1c9732058bc5306a3b955605b3071f96
Oumayma ROMDHANE <romdhaneoumayma22@gmail.com>
2021-04-09 13:14:59 +0200

    fix matrial module import

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -10,6 +10,29 @@ import {MatChipsModule} from '@angular/material/chips';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/List';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+//import {MatRippleModule} from '@angular/material/ripple';
+import {MatSelectModule} from '@angular/material/select';
+//import {MatSidenaModule} from '@angular/material/sidenav';
+//import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 const materials=[
   MatAutocompleteModule,
@@ -20,21 +43,21 @@ const materials=[
   MatChipsModule,
   MatDatepickerModule,
   MatDialogModule,
- /* MatDividerModule,
+  MatDividerModule,
   MatExpansionModule,
   MatGridListModule,
   MatIconModule,
   MatInputModule,
   MatListModule,
   MatMenuModule,
-  MatNativeDateModule,
+  //MatNativeDateModule,
   MatPaginatorModule,
   MatProgressBarModule,
   MatProgressSpinnerModule,
   MatRadioModule,
-  MatRippleModule,
+  //MatRippleModule,
   MatSelectModule,
-  MatSidenavModule,
+  //MatSidenavModule,
   MatSliderModule,
   MatSlideToggleModule,
   MatSnackBarModule,
@@ -43,7 +66,7 @@ const materials=[
   MatTableModule,
   MatTabsModule,
   MatToolbarModule,
-  MatTooltipModule*/
+  MatTooltipModule
 ];
 
 @NgModule({
